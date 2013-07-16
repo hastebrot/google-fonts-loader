7d1169e2f9e87ed62dc6048aebb217b0edec6332
hastebrot <hastebrot@gmail.com>
2013-07-16 05:18:15 +0200

    feat(css_parser): add basic code.

---
+++ src/css_parser.ts
@@ -0,0 +1,61 @@
+/// <reference path="./definitions/node.d.ts" />
+
+var _ = require("lodash")
+var fs = require("fs")
+
+export interface CssFontDefinition {
+  locals: string[];
+  urls: string[];
+}
+
+export class CssFontParser {
+  private fontFacePattern: RegExp = /font-face[\S\s]*?\{([\S\s]*?)\}/g;
+  private localPattern: RegExp = /local\(['"](\S+?)['"]\)/g;
+  private urlPattern: RegExp = /url\((.*?)\)/g;
+
+  parse(cssText: string): Array<CssFontDefinition> {
+    var fontFaces = this.parseFontFaces(cssText)
+    var fontDefinitions = []
+    _.forEach(fontFaces, (fontFace: string) => {
+      var fontDefinition = this.parseCssFontDefinition(fontFace)
+      fontDefinitions.push(fontDefinition)
+    })
+    return fontDefinitions
+  }
+
+  private parseFontFaces(cssText: string): any[] {
+    return this.match(this.fontFacePattern, cssText)
+  }
+
+  private parseCssFontDefinition(fontFace: string): CssFontDefinition {
+    var locals = this.match(this.localPattern, fontFace)
+    var urls = this.match(this.urlPattern, fontFace)
+    return {locals: locals, urls: urls}
+  }
+
+  private match(pattern: RegExp, text: string): string[] {
+    var captures = []
+    var result = pattern.exec(text)
+    while (result != null) {
+      var firstCapture = result[1]
+      captures.push(firstCapture)
+      result = pattern.exec(text)
+    }
+    return captures
+  }
+}
+
+function main() {
+  var cssFile = "./tmp/css_examples/fonts-multi-woff.css"
+  var cssText = fs.readFileSync(cssFile).toString()
+
+  var parser = new CssFontParser()
+  var fontDefinitions = parser.parse(cssText)
+  _.forEach(fontDefinitions, (fontDefinition: CssFontDefinition) => {
+    console.log(fontDefinition)
+  })
+}
+
+if (require.main === module) {
+  main()
+}
